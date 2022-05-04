82b3c4de3241794d0c02898ff73a54fe2c06d2eb
NanaOle <104324694+NanaOle@users.noreply.github.com>
2022-05-04 15:39:18 +0000

    last commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <View>
+      <Weather />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
